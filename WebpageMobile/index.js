a6404fb7a153d0c0fc4c32fddd61fd6973867168
Dasha Strigoun <dstrigoun@outlook.com>
2017-05-18 14:55:43 -0700

    silly backwards slash

--- WebpageMobile/index.js
+++ WebpageMobile/index.js
@@ -1,3 +1,4 @@
+
 /** THIS RUNS WHEN THE USERS LOGIN STATE CAHANGES **/
 
 var firebaseRef = firebase.database().ref();
@@ -5,197 +6,32 @@ var firebaseRef = firebase.database().ref();
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     
-	
+	//user is signed in
    $("#signOutBtn").text("Welcome " + firebase.auth().currentUser.email + ". Click to logout");
-  
-	
 
+   
   } else {
    
-
     // No user is signed in.
     $("#signOutBtn").text("Sign in / Sign Up");
-	$("#signOutBtn").click(
-  function(){
 
-	window.location = 'http://students.bcitdev.com/A00541112/WebpageMobile/index.html'
 
-	
-  }
-);
+	//window.location = 'http://students.bcitdev.com/A00541112/WebpageMobile/index.html'	
+
 
 
   }
 });
 
 
-/** SORTING BY EXPIRY **/
-
-function expiryOrganized() {
-
-		document.getElementById("table_body").innerHTML = "";
-	firebaseRef.orderByChild("expiry").on("child_added", snap => {
-	var name = snap.child("name").val();
-	var expiry = snap.child("expiry").val();
-	var id = snap.key;
-	var childAuthor = snap.child("author").val();
-	var thisAuthor = firebase.auth().currentUser.uid;
-
-	
-	var today = new Date();
-	var todayAgain = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth()+1; //January is 0!
-	var yyyy = today.getFullYear();
-	var ddd = today.getDate() + 2;
-	var dddd = today.getDate() + 1;
-
-	if(dd<10) {
-		dd='0'+dd
-	} 
-
-	if(mm<10) {
-		mm='0'+mm
-	} 
-
-	today = mm + '/' + dd + '/' + yyyy;
-	todayAgain = mm + '/' + ddd + '/' + yyyy;
-	todayAgainAgain = mm + '/' + dddd + '/' + yyyy;
-	
-	var daysLeft = expiry.substring(3,5) - dd;
-	
-	if (daysLeft < 0) {
-		daysLeft = 0;
-	}
-	
-	if (expiry.substring(0,2) > mm) {
-		daysLeft = expiry.substring(0,2) - mm + ' months';
-	}
-	
-	if (expiry.substring(6,10) > yyyy) {
-		daysLeft = expiry.substring(6,10) - yyyy + ' years';
-	}
-	
-	
-	if (childAuthor == thisAuthor) {
-	
-		if ( expiry <= today && expiry.substring(6,10) <= yyyy) {
-	
-	
-	var html = '<tr style="background-color: red;" id ="'+id+'"><td>'+ name + '</td><td>' + expiry + '</td><td>' + daysLeft + '</td><td><button onclick = "removeClick(this)" id ="'+id+'">Remove</button></td></tr>'; 
-	
-	$(html).appendTo('#table_body');
-	
-		} else if ( expiry == todayAgain || expiry == todayAgainAgain) {
-		
-	
-	var html = '<tr style ="background-color: yellow;" id ="'+id+'"><td>'+ name + '</td><td>' + expiry + '</td><td>' + daysLeft + '</td><td><button onclick = "removeClick(this)" id ="'+id+'">Remove</button></td></tr>'; 
-	
-	
-	$(html).appendTo('#table_body');
-		
-		
-		}else {
-			
-	var html = '<tr id ="'+id+'"><td>'+ name + '</td><td>' + expiry + '</td><td>' + daysLeft + '</td><td><button onclick = "removeClick(this)" id ="'+id+'">Remove</button></td></tr>'; 
-	
-	$(html).appendTo('#table_body');
-			
-		}
-    }
-
-	
-  });
- 
-}
 
 
 
-/** SORTING BY ALPHABET **/
-
-
-function alphabetOrganized() {
-
-
-		document.getElementById("table_body").innerHTML = "";
-	firebaseRef.orderByChild("name").on("child_added", snap => {var name = snap.child("name").val();
-	var expiry = snap.child("expiry").val();
-	var id = snap.key;
-	var childAuthor = snap.child("author").val();
-	var thisAuthor = firebase.auth().currentUser.uid;
-
-	
-	var today = new Date();
-	var todayAgain = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth()+1; //January is 0!
-	var yyyy = today.getFullYear();
-	var ddd = today.getDate() + 2;
-	var dddd = today.getDate() + 1;
-
-	if(dd<10) {
-		dd='0'+dd
-	} 
-
-	if(mm<10) {
-		mm='0'+mm
-	} 
-
-	today = mm + '/' + dd + '/' + yyyy;
-	todayAgain = mm + '/' + ddd + '/' + yyyy;
-	todayAgainAgain = mm + '/' + dddd + '/' + yyyy;
-	
-	var daysLeft = expiry.substring(3,5) - dd;
-	
-	if (daysLeft < 0) {
-		daysLeft = 0;
-	}
-	
-	if (expiry.substring(0,2) > mm) {
-		daysLeft = expiry.substring(0,2) - mm + ' months';
-	}
-	
-	if (expiry.substring(6,10) > yyyy) {
-		daysLeft = expiry.substring(6,10) - yyyy + ' years';
-	}
-	
-	
-	if (childAuthor == thisAuthor) {
-	
-		if ( expiry <= today && expiry.substring(6,10) <= yyyy) {
-	
-	
-	var html = '<tr style="background-color: red;" id ="'+id+'"><td>'+ name + '</td><td>' + expiry + '</td><td>' + daysLeft + '</td><td><button onclick = "removeClick(this)" id ="'+id+'">Remove</button></td></tr>'; 
-	
-	$(html).appendTo('#table_body');
-	
-		} else if ( expiry == todayAgain || expiry == todayAgainAgain) {
-		
-	
-	var html = '<tr style ="background-color: yellow;" id ="'+id+'"><td>'+ name + '</td><td>' + expiry + '</td><td>' + daysLeft + '</td><td><button onclick = "removeClick(this)" id ="'+id+'">Remove</button></td></tr>'; 
-	
-	
-	$(html).appendTo('#table_body');
-		
-		
-		}else {
-			
-	var html = '<tr id ="'+id+'"><td>'+ name + '</td><td>' + expiry + '</td><td>' + daysLeft + '</td><td><button onclick = "removeClick(this)" id ="'+id+'">Remove</button></td></tr>'; 
-	
-	$(html).appendTo('#table_body');
-			
-		}
-    }
-
-	
-  });
-}
-
-
-/**  SUBMITTING AN ITEM INTO THE DATABASE     **/
 
 
 
+
+/**  SUBMITTING AN ITEM INTO THE DATABASE -- On click takes the values of the two input boxes and pushes them into the database    **/
 
 
 function submitClick() {
@@ -208,19 +44,88 @@ var firebaseRef = firebase.database().ref();
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
-
-	
 	var itemText = capitalizeFirstLetter(itemName.value);
+	//don't look at the stuff ahead too closely pls
+	if(itemName.value == "Wasted" || itemName.value == "wasted") {
+		window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+		return false;
+	}
+	
+	if(itemName.value == "Baby" || itemName.value == "baby") {
+		alert('goo goo ga ga?');
+		window.location.href = "http://imgur.com/a/f7YWK";
+		return false;
+	}
+	/*
+	if(itemName.value == "Barrel Roll" || itemName.value == "barrel roll") {
+		var cssAnimation = document.createElement('style');
+		cssAnimation.type = 'text/css';
+		var rules = document.createTextNode('@-webkit-keyframes roll {'+
+			'from {-webkit-transform: rotate(0deg)}' +
+			'to { -webkit-transform: rotate(360deg) }}');
+		cssAnimation.appendChild(rules);
+		document.getElementsByTagName("head")[0].appendChild(cssAnimation);
+		var rules2 = document.createTextNode('@-moz-keyframes roll {'+
+			'from {-moz-transform: rotate(0deg)}' +
+			'to { -moz-transform: rotate(360deg) }}');
+		cssAnimation.appendChild(rules2);
+		document.getElementsByTagName("head")[1].appendChild(cssAnimation);
+		var rules3 = document.createTextNode('@keyframes roll {'+
+			'from {transform: rotate(0deg)}' +
+			'to {transform: rotate(360deg) }}');
+		cssAnimation.appendChild(rules3);
+		document.getElementsByTagName("head")[2].appendChild(cssAnimation);
+	
+		document.getElementByTagName("body").style.-moz-animation-name = "roll";
+		document.getElementByTagName("body").style.-moz-animation-duration = "4s";
+		document.getElementByTagName("body").style.-moz-animation-iteration-count = "1";
+		document.getElementByTagName("body").style.-webkit-animation-name = "roll";
+		document.getElementByTagName("body").style.-webkit-animation-duration = "4s";
+		document.getElementByTagName("body").style.-webkit-animation-iteration-count = "1"; 
+		return false;
+	}*/
+	
+	if(itemName.value == "Alex" || itemName.value == "alex") {
+		alert('Please don\'t kill me Alex.');
+		window.location.href = "http://imgur.com/a/hy2aT";
+		return false;
+	}
+	
+	if(itemName.value == "Misha" || itemName.value == "misha") {
+		alert('oh boy, you guys are in for a treat');
+		window.location.href = "https://www.youtube.com/watch?v=nZMwKPmsbWE";
+		return false;
+	}
+	
+	if(itemName.value == "Dasha" || itemName.value == "dasha") {
+		alert('wow what a cool person! So smart and funny and cute!!!!');
+		return false;
+	}
+	
+	if(itemName.value == "Simran" || itemName.value == "simran") {
+		alert('what is sleep?');
+		return false;
+	}
+	
+	if(itemName.value == "Bhagwan" || itemName.value == "bhagwan") {
+		alert('You should sleep more!!!! Try this cool soothing song!');
+		window.location.href = "https://chickenonaraft.com";
+		return false;
+	}
+	
+	
 	var expiryText = expiryName.value;
+	var expiryTextAgain = expiryText.substr(6, 9);
+	var expiryTextFinal = expiryTextAgain + "/" + expiryText.substr(0, 5);
+	
+	
+	
 	
 	var author = firebase.auth().currentUser.uid;
 	var email = firebase.auth().currentUser.email;
 	
-
-	
-	
 	firebaseRef.push({
-		'expiry': expiryText,
+		'expiry': expiryTextFinal,
 		'name': itemText,
 		'author': author,
 		'email': email
@@ -230,7 +135,7 @@ function capitalizeFirstLetter(string) {
 }
 
 
-/**  REMOVING AN ITEM FROM THE DATABSE  **/
+/**  REMOVING AN ITEM FROM THE DATABSE -- Removes the selected item from the database **/
 
 function removeClick(obj) {
 	var id = obj.id;
@@ -240,12 +145,12 @@ function removeClick(obj) {
 }
 
 
-/** LOADING THE TABLE OF ITEMS FROM DATABASE BY USER **/
-
+/** LOADING THE TABLE OF ITEMS FROM DATABASE BY USER -- pulls user specific values from the database and then populates the table **/
 
 var z = 1;
 if (z == 1) {
 firebaseRef.orderByChild("expiry").on("child_added", snap => {
+	
 	var name = snap.child("name").val();
 	var expiry = snap.child("expiry").val();
 	var id = snap.key;
@@ -269,28 +174,36 @@ firebaseRef.orderByChild("expiry").on("child_added", snap => {
 		mm='0'+mm
 	} 
 
-	today = mm + '/' + dd + '/' + yyyy;
-	todayAgain = mm + '/' + ddd + '/' + yyyy;
-	todayAgainAgain = mm + '/' + dddd + '/' + yyyy;
+	today = yyyy + '/' + mm + '/' + dd;
+	todayAgain = yyyy + '/' + mm + '/' + ddd;
+	todayAgainAgain = yyyy + '/' + mm + '/' + dddd;
 	
-	var daysLeft = expiry.substring(3,5) - dd;
+	var daysLeft;
+	var monthsLeft = expiry.substring(5,7);
+	var yearsLeft = expiry.substring(0, 4);
+	var moreYears = yearsLeft - yyyy;
 	
-	if (daysLeft < 0) {
-		daysLeft = 0;
+	
+	
+	if(yearsLeft > yyyy && monthsLeft >= mm){
+		
+		daysLeft = "~year";
+		
+	} else if(yearsLeft > yyyy ) {
+		
+		daysLeft = "~" + (monthsLeft - mm + 12) + " months";
+		
+	} else {
+		
+		daysLeft = expiry.substring(8, 10) - dd;
 	}
 	
-	if (expiry.substring(0,2) > mm) {
-		daysLeft = expiry.substring(0,2) - mm + ' months';
-	}
-	
-	if (expiry.substring(6,10) > yyyy) {
-		daysLeft = expiry.substring(6,10) - yyyy + ' years';
-	}
+
 	
 	
 	if (childAuthor == thisAuthor) {
 	
-		if ( expiry <= today && expiry.substring(6,10) <= yyyy) {
+		if ( expiry <= today ) {
 	
 	
 	var html = '<tr style="background-color: red;" id ="'+id+'"><td>'+ name + '</td><td>' + expiry + '</td><td>' + daysLeft + '</td><td><button onclick = "removeClick(this)" id ="'+id+'">Remove</button></td></tr>'; 
@@ -314,16 +227,64 @@ firebaseRef.orderByChild("expiry").on("child_added", snap => {
 			
 		}
     }
-
 	
-  });
-  
+	/** start of datables**/
+	$(document).ready(function() {
+    var table = $('#fridge').DataTable();
+	
+	
+	//var classHighlight = 'highlight'; 
+
+	//.click() will return the result of $('.thumbnail')
+	//I save it for future reference so I don't have to query the DOM again
+	
+	
+	/*
+	var $fridge = $('tr').click(function(e) {
+		e.preventDefault();
+		//run removeClass on every element
+		//if the elements are not static, you might want to rerun $('.thumbnail')
+		//instead of the saved $thumbs
+		//$fridge.removeClass(classHighlight);
+		//add the class to the currently clicked element (this)
+		
+		if (!$(this).hasClass('highlight')) {
+			//alert('this is NOT highlighted');
+			$(this).addClass('highlight');
+		}  
+		
+	});
+	
+	*/
+    
+	} );
+	/** end of datables **/
+	
+	
+  }); 
 
 }
 
 
+/** click functionality **/
+	
+$(document).ready(function() {
+    $('#fridge tbody').on('click', 'tr', function() {
+		if($(this).hasClass('highlight')) {
+			$(this).removeClass('highlight');
+		} else {
+			$(this).addClass('highlight');
+		} 
+    });
+});
+	
 
-/* LOGOUT PROCESS */
+
+
+	
+
+
+/* LOGOUT PROCESS -- Signs out the current user */
 
 $("#signOutBtn").click(
   function(){
@@ -341,3 +302,6 @@ $("#signOutBtn").click(
 	
   }
 );
+
+
+
